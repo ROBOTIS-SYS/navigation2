7213bc60d45169061bad0a8bdb1f5fcbdbd8c359
Kayman <kmjung@robotis.com>
2021-09-14 14:32:23 +0900

    prefer direction critic : 디버그 로그 수정

--- nav2_dwb_controller/dwb_critics/src/prefer_direction.cpp
+++ nav2_dwb_controller/dwb_critics/src/prefer_direction.cpp
@@ -63,15 +63,18 @@ void PreferDirectionCritic::onInit()
 
 double PreferDirectionCritic::scoreTrajectory(const dwb_msgs::msg::Trajectory2D & traj)
 {
+  std::cout << "Tra vel | x : " << traj.velocity.x << ", y : " << traj.velocity.y <<
+    ", theta : " << traj.velocity.theta << std::endl;
+
   if (prefer_right_ && traj.velocity.theta > 0.0) {
-    std::cout << "[PreferDirection]|Right| theta : " << traj.velocity.theta << ", penalty : " <<
-      penalty_;
+//    std::cout << "[PreferDirection]|Right| theta : " << traj.velocity.theta << ", penalty : " <<
+//      penalty_ << std::endl;
     return penalty_;
   }
 
   if (!prefer_right_ && traj.velocity.theta < 0.0) {
-    std::cout << "[PreferDirection]|Left| theta : " << traj.velocity.theta << ", penalty : " <<
-      penalty_;
+//    std::cout << "[PreferDirection]|Left| theta : " << traj.velocity.theta << ", penalty : " <<
+//      penalty_ << std::endl;
     return penalty_;
   }
 
