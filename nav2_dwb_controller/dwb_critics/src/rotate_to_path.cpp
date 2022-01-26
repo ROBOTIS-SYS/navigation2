eb590657a0079a7eb1f73811528e6af12de2bc89
Kayman <kmjung@robotis.com>
2022-01-26 13:56:28 +0900

    rotate_to_path critic 추가, 테스트 코드 구현

---
+++ nav2_dwb_controller/dwb_critics/src/rotate_to_path.cpp
@@ -0,0 +1,162 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2017, Locus Robotics
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the copyright holder nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "dwb_critics/rotate_to_path.hpp"
+#include <string>
+#include <vector>
+#include "nav_2d_utils/parameters.hpp"
+#include "dwb_core/exceptions.hpp"
+#include "pluginlib/class_list_macros.hpp"
+#include "dwb_core/trajectory_utils.hpp"
+#include "angles/angles.h"
+
+PLUGINLIB_EXPORT_CLASS(dwb_critics::RotateToPathCritic, dwb_core::TrajectoryCritic)
+
+namespace dwb_critics
+{
+
+inline double hypot_sq(double dx, double dy)
+{
+  return dx * dx + dy * dy;
+}
+
+void RotateToPathCritic::onInit()
+{
+  double theta_tolerance_deg = nav_2d_utils::searchAndGetParam(
+    nh_,
+    dwb_plugin_name_ + ".theta_tolerance", 60.0);
+  theta_tolerance_ = theta_tolerance_deg * M_PI / 180.0;
+  theta_tolerance_sq_ = theta_tolerance_ * theta_tolerance_;
+  double in_window_xy_velocity = nav_2d_utils::searchAndGetParam(
+    nh_,
+    dwb_plugin_name_ + ".trans_in_window_velocity", 0.25);
+  in_window_xy_velocity_sq_ = in_window_xy_velocity * in_window_xy_velocity;
+  slowing_factor_ = nav_2d_utils::searchAndGetParam(
+    nh_,
+    dwb_plugin_name_ + "." + name_ + ".slowing_factor", 5.0);
+  lookahead_time_ = nav_2d_utils::searchAndGetParam(
+    nh_,
+    dwb_plugin_name_ + "." + name_ + ".lookahead_time", -1.0);
+  lokahead_distance_ = nav_2d_utils::searchAndGetParam(
+    nh_,
+    dwb_plugin_name_ + "." + name_ + ".lookahead_distance", 0.25);
+  reset();
+}
+
+void RotateToPathCritic::reset()
+{
+  in_window_ = false;
+  rotating_ = false;
+}
+
+bool RotateToPathCritic::prepare(
+  const geometry_msgs::msg::Pose2D & pose, const nav_2d_msgs::msg::Twist2D & vel,
+  const geometry_msgs::msg::Pose2D & goal,
+  const nav_2d_msgs::msg::Path2D & global_plan)
+{
+  double diff_x = 0.0;
+  double diff_y = 0.0;
+  double diff_theta = 0.0;
+
+  for (int ix = 0; ix < global_plan.poses.size(); ix++) {
+    double temp_diff_x = global_plan.poses.at(ix).x - pose.x;
+    double temp_diff_y = global_plan.poses.at(ix).y - pose.y;
+    if (lokahead_distance_ < sqrt(temp_diff_x * temp_diff_x + temp_diff_y * temp_diff_y)) {
+      diff_x = temp_diff_x;
+      diff_y = temp_diff_y;
+      diff_theta = atan2(diff_y, diff_x) - pose.theta;
+      break;
+    }
+  }
+
+  if (diff_x == 0.0 && diff_y == 0.0) {
+    in_window_ = false;
+    rotating_ = false;
+    return true;
+  }
+
+  if (fabs(diff_theta) < theta_tolerance_) {
+    in_window_ = false;
+    rotating_ = false;
+  } else {
+    in_window_ = true;
+  }
+
+//  double dxy_sq = hypot_sq(pose.x - goal.x, pose.y - goal.y);
+//  in_window_ = in_window_ || fabs(diff_theta) > theta_tolerance_;
+  current_xy_speed_sq_ = hypot_sq(vel.x, vel.y);
+  rotating_ = rotating_ || (in_window_ && current_xy_speed_sq_ <= in_window_xy_velocity_sq_);
+  goal_yaw_ = diff_theta;
+  return true;
+}
+
+double RotateToPathCritic::scoreTrajectory(const dwb_msgs::msg::Trajectory2D & traj)
+{
+  // If we're not sufficiently close to the goal, we don't care what the twist is
+  if (!in_window_) {
+    return 0.0;
+  } else if (!rotating_) {
+    double speed_sq = hypot_sq(traj.velocity.x, traj.velocity.y);
+//    if (speed_sq >= current_xy_speed_sq_) {
+//      throw dwb_core::IllegalTrajectoryException(name_, "Not slowing down near goal.");
+//    }
+    return speed_sq * slowing_factor_ + scoreRotation(traj);
+  }
+
+  // If we're sufficiently close to the goal, any transforming velocity is invalid
+//  if (fabs(traj.velocity.x) > 0 || fabs(traj.velocity.y) > 0) {
+//    throw dwb_core::
+//          IllegalTrajectoryException(name_, "Nonrotation command near goal.");
+//  }
+
+  return scoreRotation(traj);
+}
+
+double RotateToPathCritic::scoreRotation(const dwb_msgs::msg::Trajectory2D & traj)
+{
+  if (traj.poses.empty()) {
+    throw dwb_core::IllegalTrajectoryException(name_, "Empty trajectory.");
+  }
+
+  double end_yaw;
+  if (lookahead_time_ >= 0.0) {
+    geometry_msgs::msg::Pose2D eval_pose = dwb_core::projectPose(traj, lookahead_time_);
+    end_yaw = eval_pose.theta;
+  } else {
+    end_yaw = traj.poses.back().theta;
+  }
+  return fabs(angles::shortest_angular_distance(end_yaw, goal_yaw_));
+}
+
+}  // namespace dwb_critics
